291b7897c1f58e2d7c7f7948007a80a7e1a28559
SamWilk <samwilk18@yahoo.com>
2022-04-29 16:21:29 -0400

    Creating img component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <img
+    alt="Sam Wilk"
+    src="./assets/picture-Sam/Profile-pic.jpeg"
+    height="100"
+    width="100"
+    class="photo"
+  /> -->
+  <PictureComp
+    imageFile="./assets/picture-Sam/Profile-pic.jpeg"
+    msg="Sam Wilk"
+  />
   <HelloWorld msg="Welcome to Your Vue.js App" />
   <div>Hello There I am testing out git</div>
   <AboutMe name="Sam Wilk" />
@@ -8,12 +19,14 @@
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import AboutMe from "./components/AboutMe.vue";
+import PictureComp from "./components/PictureComp.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
     AboutMe,
+    PictureComp,
   },
 };
 </script>
---
+++ src/components/PictureComp.vue
@@ -0,0 +1,24 @@
+<template>
+  <img alt="" :src="getImgSrc()" width="100" height="100" class="myPic" />
+</template>
+
+<script>
+export default {
+  name: "PictureComp",
+  props: {
+    msg: String,
+    imageFile: String,
+  },
+  methods: {
+    getImgSrc() {
+      return console.log("Image", this.imageFile) || this.imageFile;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.myPic {
+  border-radius: 12px;
+}
+</style>
