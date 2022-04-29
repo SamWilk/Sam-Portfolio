36df70db16fac92f0cc9b40224dd69b3270b1966
SamWilk <samwilk18@yahoo.com>
2022-04-29 12:17:10 -0400

    Adding to main branch

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>Hello There I am testing out git</div>
+  <AboutMe name="Sam Wilk" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import AboutMe from "./components/AboutMe.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    AboutMe,
+  },
+};
 </script>
 
 <style>
@@ -20,6 +24,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  font-family: "Roboto", sans-serif;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/AboutMe.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>This component is about {{ name }}</div>
+</template>
+
+<script>
+export default {
+  name: "AboutMe",
+  props: {
+    name: String,
+  },
+};
+</script>
